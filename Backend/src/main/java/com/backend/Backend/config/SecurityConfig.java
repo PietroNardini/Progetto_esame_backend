f484a903797bd3c526a572011b7b0bfdeb6b651b
PietroNardini <pietro01.nardini@edu.unife.it>
2025-05-15 21:21:14 +0200

    added securityConfig

---
+++ Backend/src/main/java/com/backend/Backend/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.backend.Backend.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors() // Enable CORS
+            .and()
+            .csrf().disable(); // Disable CSRF for simplicity
+
+        return http.build();
+    }
+}
